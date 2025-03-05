de2715b971b28bac373189160d22c45757576e8a
Shan Yan <shan.yan@kellogg.northwestern.edu>
2025-03-05 23:00:02 +0000

    completed login, user authentication and security

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user["email"] != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "welcome, #{@user["first_name"]}"
+        redirect_to "/companies"
+      else 
+        flash["notice"] = "login failed. Please try again"
+        redirect_to "/login"
+      end
+    else 
+      flash["notice"] = "login failed. Please try again"
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Bye"
+    redirect_to "/login"
+  end
 end
